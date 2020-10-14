fef06cb6853be94881f19e2855e085d9436484a1
Tiago Berwanger <berwangertiago@gmail.com>
2020-10-14 00:40:23 -0300

    finishing map page

---
+++ public/script/page-orphanages.js
@@ -0,0 +1,26 @@
+//create map
+const map = L.map('mapid').setView([-27.5953637,-48.540218], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [38, 48],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2],
+})
+
+//create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+
+//create and add marker
+L.marker([-27.5953637,-48.540218], {icon})
+.addTo(map)
+.bindPopup(popup)
