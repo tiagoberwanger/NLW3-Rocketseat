e077f264606434efd3b388bc19ace478686f9099
Tiago Berwanger <berwangertiago@gmail.com>
2020-10-16 11:59:52 -0300

    Hello World from backend

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+//importar dependência
+const express = require('express');
+const path = require('path');
+
+//iniciando a biblioteca express
+const server = express();
+
+server
+    //utilizando arquivos estáticos 
+    .use(express.static('public'))
+
+    //criar rota
+    .get('/', (request, response) => {
+        return response.sendFile(path.join(__dirname, 'views', 'index.html'));
+    })
+
+//ligar o servidor
+server.listen(5500);
